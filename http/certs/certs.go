010fdafb5e17fb47f8bde10989a7193063d9c8e6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-17 12:29:00 -0500

    cert testing

--- http/certs/certs.go
+++ http/certs/certs.go
@@ -1,5 +1,9 @@
 package certs
 
+import (
+	"os"
+)
+
 type CertFile struct {
 	certFile string
 	keyFile  string
@@ -14,4 +18,32 @@ func NewCertFile() *CertFile {
 func (cf *CertFile) AddPath(path string) *CertFile {
 	cf.path = append(cf.path, path)
 	return cf
+}
+
+func (cf *CertFile) ClearPath() {
+	cf.path = cf.path[:0]
+}
+
+func (cf *CertFile) KeyFile() (string, error) {
+	for _, path := range cf.path {
+		filename := path + "/" + cf.keyFile
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			continue
+		}
+		return filename, nil
+	}
+
+	return "", os.ErrNotExist
+}
+
+func (cf *CertFile) CertFile() (string, error) {
+	for _, path := range cf.path {
+		filename := path + "/" + cf.certFile
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			continue
+		}
+		return filename, nil
+	}
+
+	return "", os.ErrNotExist
 }
--- http/certs/certs_test.go
+++ http/certs/certs_test.go
@@ -1,10 +1,49 @@
 package certs
 
-import "testing"
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
 
-func TestCertFile_AddPath(t *testing.T) {
+func TestCertFile_KeyFile(t *testing.T) {
 
 	cf := NewCertFile()
-	cf.AddPath("a").AddPath("b")
+	cf.AddPath("./fixtures").AddPath("./http/certs/fixtures")
 
+	file, err := cf.KeyFile()
+	if err != nil {
+		t.Fatalf("Can't get file")
+	}
+	dat, err := ioutil.ReadFile(file)
+	if !strings.Contains(string(dat), "dummy tls.key") {
+		t.Fatalf("Bad info in file")
+	}
+
+	cf.ClearPath()
+	file, err = cf.KeyFile()
+	if err == nil {
+		t.Fatalf("not working")
+	}
+}
+
+func TestCertFile_CertFile(t *testing.T) {
+
+	cf := NewCertFile()
+	cf.AddPath("./fixtures").AddPath("./http/certs/fixtures")
+
+	file, err := cf.CertFile()
+	if err != nil {
+		t.Fatalf("Can't get file")
+	}
+	dat, err := ioutil.ReadFile(file)
+	if !strings.Contains(string(dat), "dummy tls.cert") {
+		t.Fatalf("Bad info in file")
+	}
+
+	cf.ClearPath()
+	file, err = cf.KeyFile()
+	if err == nil {
+		t.Fatalf("not working")
+	}
 }
